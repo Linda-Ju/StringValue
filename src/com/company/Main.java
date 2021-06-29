0699b1c33eae6f56f801de4f558984775ccc9799
Linda-Ju <linda.junkina@gmail.com>
2021-06-30 00:31:49 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String ten = "10";
+        String five = "5";
+        int ten10 = Integer.parseInt(ten);
+        int five5 = Integer.parseInt(five);
+        int sum = ten10 + five5;
+        System.out.println(sum);
+    }
+}
